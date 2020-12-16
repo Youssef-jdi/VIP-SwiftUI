b1da7d628c134ba6914d6cf855e58af4d216f2b4
Youssef Jdidi <youssef.jdidi@esprit.tn>
2020-12-16 12:58:52 +0100

    Initial Commit

---
+++ VIP-SwiftUI/VIP_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIP_SwiftUIApp.swift
+//  VIP-SwiftUI
+//
+//  Created by Youssef Jdidi on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct VIP_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
